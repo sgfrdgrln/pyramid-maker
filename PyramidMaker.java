2468bac17d40922036b62af57ff3e27c06c39154
sgfrdgrln <gazel0123@gmail.com>
2024-05-01 13:10:45 +0800

    added another option

--- PyramidMaker.java
+++ PyramidMaker.java
@@ -6,35 +6,101 @@ public class PyramidMaker {
         Scanner userInput = new Scanner(System.in);
         
         int height;
-        boolean validHeight = false;
-        
-        while(!validHeight) {
+        boolean validHeight = false, validChoice = false;
+        int choice;
+
+        while(!validChoice) {
+            validHeight = false;
             try {
-                
                 do {
-                    System.out.println("\nPyramid Maker\n");
-                    System.out.print("Enter height (1-8): ");
-                    height = userInput.nextInt();
-                    
-                    if (height > 8 || height < 1) {
-                        System.out.println("Invalid height.");
+                    System.out.println("\nThe Pyramid Maker\n");
+                    System.out.println("1. Pyramid Maker w/ gap\n2. Normal Pyramid Maker\n3. Stop the Program\n");
+                    System.out.print("Choose a number: ");
+                    choice = userInput.nextInt();
+
+                }while(choice > 3 || choice < 1);
+
+                switch (choice) {
+                    case 1:
+
+                    while(!validHeight) {
+                        try {
+                            
+                            do {
+                                System.out.println("\nPyramid Maker w/ gap :D\n");
+                                System.out.print("Enter height (1-8): ");
+                                height = userInput.nextInt();
+                                
+                                if (height > 8 || height < 1) {
+                                    System.out.println("Invalid height.");
+                                }
+                                
+                                
+                            }while(height > 8 || height < 1);
+                            
+                            validHeight = true;
+                            getHeightOfPyramid(height);
+                            
+                        }catch(InputMismatchException err) {
+                            System.out.println("Invalid height.");
+                            userInput.nextLine();
+                        }
                     }
                     
+                        break;
+                    case 2:
+
+                    while(!validHeight) {
+                        try {
+                            
+                            do {
+                                System.out.println("\nNormal Pyramid Maker\n");
+                                System.out.print("Enter height (1-8): ");
+                                height = userInput.nextInt();
+                                
+                                if (height > 8 || height < 1) {
+                                    System.out.println("Invalid height.");
+                                }
+                                
+                                
+                            }while(height > 8 || height < 1);
+                            
+                            validHeight = true;
+                            getHeightOfPyramid2(height);
+                            
+                        }catch(InputMismatchException err) {
+                            System.out.println("Invalid height.");
+                            userInput.nextLine();
+                        }
+                    }
+
+                        break;
                     
-                }while(height > 8 || height < 1);
-                
-                validHeight = true;
-                get_height(height);
-                
-            }catch(InputMismatchException err) {
-                System.out.println("Invalid height.");
+                    case 3:
+                        System.out.println("Program stopped.");
+                        validChoice = true;
+                        break;
+                    
+                    default:
+
+                    System.out.println("Invalid choice.");
+                        break;
+                }
+            }
+            catch(InputMismatchException err) {
+
+                System.out.println("Invalid choice.");
                 userInput.nextLine();
             }
         }
+        
+        
+        
+        
      
          
     }
-    public static void get_height(int height) {
+    public static void getHeightOfPyramid(int height) {
         
     int rows = height;
     int gapWidth = 2;
@@ -62,6 +128,25 @@ public class PyramidMaker {
          System.out.println();
      }
         
-    }    
+    }  
+    public static void getHeightOfPyramid2(int height){
+
+        int rows = height, blocks = 0;
+
+        System.out.println("\nYour Pyramid:\n");
+
+        for (int i = 1; i <= rows; i++, blocks = 0) {
+            for(int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            while(blocks != 2 * i - 1) {
+                System.out.print("# ");
+                blocks++;
+            }
+            System.out.println();
+        }
+
+    }  
     
 }
